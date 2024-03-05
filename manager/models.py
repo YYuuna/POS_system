48bd4a9e5757f7d0dd45aa699b2cecbbf33eb514
YYuuna <75922571+YYuuna@users.noreply.github.com>
2024-03-05 10:14:50 +0100

    Refactoring models to french

--- manager/models.py
+++ manager/models.py
@@ -1,128 +1,120 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-class Supplier(models.Model):
-    name = models.CharField(max_length=100)
-    phone = PhoneNumberField(unique=True)
+class Fournisseur(models.Model):
+    nom = models.CharField(max_length=100)
+    tel = PhoneNumberField(unique=True)
     email = models.EmailField(unique=True)
     address = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.nom
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone = PhoneNumberField(unique=True)
+    prenom = models.CharField(max_length=100)
+    nom = models.CharField(max_length=100)
+    tel = PhoneNumberField(unique=True)
     email = models.EmailField(unique=True)
-    address = models.TextField()
+    adresse = models.TextField()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.prenom} {self.nom}"
 
 
-class Product(models.Model):
-    CATEGORY_CHOICES = [
-        ('TÉLÉPHONE', 'Téléphone'),
-        ('PC', 'PC'),
-        ('TABLETTE', 'Tablette'),
-        ('ACCESSOIRE', 'Accessoire'),
-        ('APPAREIL_PHOTO', 'Appareil photo'),
-        ('AUTRE', 'Autre')
-    ]
+class Produit(models.Model):
 
-    STATUS_CHOICES = [
+    CHOIX_ETAT = [
         ('EN_VENTE', 'En vente'),
         ('EN_RÉPARATION', 'En réparation'),
         ('RÉPARATION_TERMINÉE', 'Réparation terminée')
     ]
 
-    name = models.CharField(max_length=100)
+    nom = models.CharField(max_length=100)
     description = models.TextField()
-    initial_buying_price = models.DecimalField(max_digits=10, decimal_places=2)
-    initial_selling_price = models.DecimalField(max_digits=10, decimal_places=2)
-    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, blank=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='EN_VENTE')
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    prix_achat_initiale = models.DecimalField(max_digits=10, decimal_places=2)
+    prix_vente_initiale = models.DecimalField(max_digits=10, decimal_places=2)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.SET_NULL, null=True, blank=True)
+    etat = models.CharField(max_length=20, choices=CHOIX_ETAT, default='EN_VENTE')
+    
     # Other fields...
 
     def __str__(self):
-        return self.name
+        return self.nom
 
 
-class PurchaseOrder(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, blank=True)
-    order_date = models.DateField(auto_now_add=True)
-    delivery_date = models.DateField(null=True, blank=True)
-    is_delivered = models.BooleanField(default=False)
+class Commande(models.Model):
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.SET_NULL, null=True, blank=True)
+    date_commande = models.DateField(auto_now_add=True)
+    date_delivraison = models.DateField(null=True, blank=True)
+    delivre = models.BooleanField(default=False)
     # Other fields...
 
     def __str__(self):
-        return f"Purchase Order #{self.pk} - Supplier: {self.supplier.name}"
+        return f"Purchase Order #{self.pk} - Supplier: {self.fournisseur.nom}"
 
 
-class PurchaseOrderItem(models.Model):
-    order = models.ForeignKey(PurchaseOrder, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)  # Dynamic price for purchase order
+class ArticleCommande(models.Model):
+    commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
+    prix_achat = models.DecimalField(max_digits=10, decimal_places=2)  # Dynamic price for purchase order
     # Other fields...
 
     def __str__(self):
-        return f"Purchase Order Item #{self.pk} - Product: {self.product.name}"
+        return f"Purchase Order Item #{self.pk} - produit: {self.produit.nom}"
 
     class Meta:
-        unique_together = ('order', 'product')
+        unique_together = ('order', 'produit')
 
 
-class Sale(models.Model):
+class Vente(models.Model):
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
-    sale_date = models.DateField(auto_now_add=True)
+    date_achat = models.DateField(auto_now_add=True)
     # Other fields...
 
     def __str__(self):
-        return f"Sale #{self.pk} - Client: {self.client.first_name} {self.client.last_name}"
+        return f"Sale #{self.pk} - Client: {self.client.prenom} {self.client.nom}"
 
 
-class SaleItem(models.Model):
-    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    sale_price = models.DecimalField(max_digits=10, decimal_places=2)  # Dynamic price for sale
+class ArticleVente(models.Model):
+    vente = models.ForeignKey(Vente, on_delete=models.CASCADE)
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    quantite = models.IntegerField()
+    prix_vente = models.DecimalField(max_digits=10, decimal_places=2)  # Dynamic price for sale
     # Other fields...
 
     def __str__(self):
-        return f"Sale Item #{self.pk} - Product: {self.product.name}"
+        return f"Sale Item #{self.pk} - produit: {self.produit.nom}"
 
     class Meta:
-        unique_together = ('sale', 'product')
+        unique_together = ('sale', 'produit')
 
 
-class Repair(models.Model):
-    STATUS_CHOICES = [
+class Reparation(models.Model):
+    CHOIX_ETAT = [
         ('EN_COURS', 'En cours'),
         ('TERMINÉ', 'Terminé')
     ]
 
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    produit = models.ForeignKey(Produit, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField()
-    repair_date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='EN_COURS')
+    date_reparation = models.DateField(auto_now_add=True)
+    etat = models.CharField(max_length=20, choices=CHOIX_ETAT, default='EN_COURS')
     # Other fields...
 
     def __str__(self):
-        return f"Repair for Product: {self.product.name}, Client: {self.client.first_name} {self.client.last_name}"
+        return f"Reparation pour produit: {self.produit.nom}, Client: {self.client.prenom} {self.client.nom}"
 
 
-class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    phone = PhoneNumberField(unique=True)
+class Employe(models.Model):
+    prenom = models.CharField(max_length=100)
+    nom = models.CharField(max_length=100)
+    salaire = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    tel = PhoneNumberField(unique=True)
     email = models.EmailField(unique=True)
-    address = models.TextField()
+    adresse = models.TextField()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.prenom} {self.nom}"
