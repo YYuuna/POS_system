2d5dae511fb856313cc3d345f98f08ef01467624
mourad <mouradlakamoura4@gmail.com>
2024-03-12 15:03:10 +0100

    liste de fournisseur

--- manager/urls.py
+++ manager/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('ajouter-client', views.AddClientView.as_view(), name='add-client'),
     path('login',views.UserLoginView.as_view(),name='login'),
     path('clients/', views.ClientListView.as_view(), name='client-list'),
-
+   
 ]
